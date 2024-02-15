95993de336dd88f693d35c28488e8dcb62d2d9d8
Kateryna1706 <119225233+Kateryna1706@users.noreply.github.com>
2024-02-15 18:03:37 +0000

    Deploying to gh-pages from @ Kateryna1706/vocab-builder@3e721f2f98a84399adbffe10c31c3358065a8284 🚀

---
+++ static/js/448.e81be1db.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvocab_builder=self.webpackChunkvocab_builder||[]).push([[448],{2448:(s,e,a)=>{a.r(e),a.d(e,{default:()=>u});var c=a(184);const d=()=>(0,c.jsx)("div",{}),i=()=>(0,c.jsx)("div",{}),u=()=>(0,c.jsxs)("main",{children:[(0,c.jsx)(d,{}),(0,c.jsx)(i,{})]})}}]);
+//# sourceMappingURL=448.e81be1db.chunk.js.map
